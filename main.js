0408b07021ae39402eccfb8e27d87ac57d9584a6
Chris Bieganek <cwbieganek@gmail.com>
2021-11-13 08:05:07 -0600

    Move app code to add.mjs module

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import { sum } from "./math.mjs"; 
+import app from "./app.mjs";
 
-new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
-});
+app();
