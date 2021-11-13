a6ef660946bbf1227ca3ba5bc67b660bd2568015
Chris Bieganek <cwbieganek@gmail.com>
2021-11-13 17:23:16 -0600

    Properly copy only index.html and index.css

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
 	plugins: [
 		new CopyPlugin({
 			patterns: [
-				{ from: path.resolve(__dirname, "src") + "/*.html", to: "dist" },
-				{ from: path.resolve(__dirname, "src") + "/*.css", to: "dist" }
+				{ from: path.resolve(__dirname, "src", "index.html"), to: path.resolve(__dirname, "dist") },
+				{ from: path.resolve(__dirname, "src", "index.css"), to: path.resolve(__dirname, "dist") }
 			],
 		}),
 	],
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-  entry: path.resolve(__dirname, "src") + "/main.js",
+  entry: path.resolve(__dirname, "src", "main.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
