9ada1f20f0b54ccaf7c40ed921beb45db27dcdea
Chris Bieganek <cwbieganek@gmail.com>
2021-11-13 15:28:55 -0600

    Configure webpack-copy-plugin to copy HTML and CSS from src to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: path.resolve(__dirname, "src") + "/*.html", to: "dist" },
+				{ from: path.resolve(__dirname, "src") + "/*.css", to: "dist" }
+			],
+		}),
+	],
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-  entry: './main.js',
+  entry: path.resolve(__dirname, "src") + "/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
